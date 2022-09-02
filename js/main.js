f321b018ea68c6eb09fd029e613b78a620005bb8
ngf93 <nabiullinaguzel93@gmail.com>
2022-09-02 14:21:44 +0300

    Feature: Quiz

--- js/main.js
+++ js/main.js
@@ -60,6 +60,57 @@ function toggleCatalog(btn){
 }
 
 
+const cart = document.getElementById('cart-preview');
+function openCartPreview(){
+  cart.style.display = 'block';
+  cart.animate({
+    "opacity": "1",
+  }, {
+    duration: 200,
+    fill: "forwards",
+  });
+}
+function closeCartPreview(){
+  cart.animate({
+    "opacity": "0",
+  }, {
+    duration: 200,
+    fill: "forwards",
+  });
+  cart.style.display = 'none';
+}
+
+
+/* inputs verification in QUIZ */
+function verifyInput(fieldset){
+  let requiredElems = Array.from(fieldset.querySelectorAll('input[required]'));
+  if(requiredElems.length == 0){
+    return;
+  } else {
+    let flag = requiredElems.every(notNull);
+    if (flag){
+      fieldset.querySelector('.verifiable-btn').removeAttribute('disabled');
+    } else {
+      fieldset.querySelector('.verifiable-btn').setAttribute('disabled', 'disabled');
+    }
+  }
+  function notNull(element) {
+    if(element.type == 'radio' || element.type == 'checkbox'){
+      let name = element.name;
+      let arrBtns = Array.from(fieldset.querySelectorAll('input[name="'+name+'"]'));
+      if(arrBtns.some(isChecked)){return element;}
+    } else if (element.value.trim() != ''){
+      return element;
+    }
+  }
+  function isChecked(el){
+    if(el.checked && el.value.trim() != ''){
+      return el;
+    }
+  }
+}
+
+
 /*************** 
 CUSTOM SELECT
 ****************/
